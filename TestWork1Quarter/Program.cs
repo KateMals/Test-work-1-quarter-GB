21ca8d9e308090b85d16760dd352b6fe6fa5327f
Kate Malanina <katpm07@gmail.com>
2023-03-30 11:46:25 +0300

    strings added

--- TestWork1Quarter/Program.cs
+++ TestWork1Quarter/Program.cs
@@ -1,24 +1,2 @@
 ﻿string[] array1 = new string[4] {"Russia", "Denmark", "Kazan", "Ufa"};
 string[] newArray = new string[array1.Length];
-void ArrayOf3Simbols(string[] array1, string[] array2)
-{
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
-    {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
-        }
-    }
-}
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-ArrayOf3Simbols(array1, newArray);
-PrintArray(newArray);
