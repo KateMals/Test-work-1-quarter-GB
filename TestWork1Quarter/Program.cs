ebd068bf23dbbe6fd7cc0c9b5879de7b9f7dddbf
Kate Malanina <katpm07@gmail.com>
2023-03-31 10:37:58 +0300

    TestWork Ended

--- TestWork1Quarter/Program.cs
+++ TestWork1Quarter/Program.cs
@@ -1,13 +1,13 @@
 ﻿string[] array1 = new string[4] {"Russia", "Denmark", "Kazan", "Ufa"};
 string[] newArray = new string[array1.Length];
-void ArrayOf3Simbols(string[] array1, string[] array2)
+void ArrayOf3Simbols(string[] array1, string[] newArray)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
     if(array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
+        newArray[count] = array1[i];
         count++;
         }
     }
